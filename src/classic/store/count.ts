ee545ef522825b2e2821429932378b1ba7179eef
Igor Zvyagin <spirit-drive@yandex.ru>
2023-11-12 13:32:29 +0300

    improve count reducer for classic redux store

--- src/classic/store/count.ts
+++ src/classic/store/count.ts
@@ -1,9 +1,35 @@
 import { ClassicState } from 'src/classic/store/index';
 
-// Actions
 const INCREASE = 'count/increase';
 const DECREASE = 'count/decrease';
 const SET = 'count/set';
+
+type CountAction = IncreaseAction | DecreaseAction | SetAction;
+
+const initialState = 0;
+
+export const count = (state = initialState, action: CountAction): number => {
+  switch (action.type) {
+    case INCREASE:
+      return state + action.payload.amount;
+    case DECREASE:
+      return state - action.payload.amount;
+    case SET:
+      return action.payload.value;
+    default:
+      return state;
+  }
+};
+
+export const countActions = {
+  decrease: (payload: DecreaseAction['payload']): DecreaseAction => ({ type: DECREASE, payload }),
+  set: (payload: SetAction['payload']): SetAction => ({ type: SET, payload }),
+  increase: (payload: IncreaseAction['payload']): IncreaseAction => ({ type: INCREASE, payload }),
+};
+
+export const countSelectors = {
+  get: (state: ClassicState) => state.count,
+};
 
 interface IncreaseAction {
   type: typeof INCREASE;
@@ -25,30 +51,3 @@ interface SetAction {
     value: number;
   };
 }
-
-export const countActions = {
-  decrease: (payload: DecreaseAction['payload']): DecreaseAction => ({ type: DECREASE, payload }),
-  set: (payload: SetAction['payload']): SetAction => ({ type: SET, payload }),
-  increase: (payload: IncreaseAction['payload']): IncreaseAction => ({ type: INCREASE, payload }),
-};
-
-type CountAction = IncreaseAction | DecreaseAction | SetAction;
-
-// Reducer
-const initialState = 0;
-
-export const count = (state = initialState, action: CountAction): number => {
-  switch (action.type) {
-    case INCREASE:
-      return state + action.payload.amount;
-    case DECREASE:
-      return state - action.payload.amount;
-    case SET:
-      return action.payload.value;
-    default:
-      return state;
-  }
-};
-
-// Selectors
-export const getCount = (state: ClassicState) => state.count;
