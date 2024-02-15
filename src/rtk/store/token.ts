9dd6441522c6684d8e9deef744d2d1f1c50e71c8
Kirill <kirill@MacBook-Pro.local>
2024-02-16 01:01:44 +0600

    Добавлен пример с extraReducer в tokenSlice

--- src/rtk/store/token.ts
+++ src/rtk/store/token.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RtkState } from 'src/rtk/store/index';
+import { countActions } from '../store/count';
 
 const tokenSlice = createSlice({
   name: 'token',
@@ -7,6 +8,19 @@ const tokenSlice = createSlice({
   reducers: {
     gen: () => Math.random().toString(16),
     same: (state) => state,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(countActions.increase, (state, action) => {
+      console.log('extraReducers: ', action, state);
+
+      return Math.random().toString(16);
+    });
+
+    builder.addCase('count/decrease', (state, action) => {
+      console.log('extraReducers: ', action, state);
+
+      return state;
+    });
   },
 });
 export const tokenActions = tokenSlice.actions;
